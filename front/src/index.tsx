831c9bd78cfd4f16a00dcdcdb8343e47cf18a2ea
thibault-barrat <contact@thibault-barrat.com>
2022-02-10 16:15:52 +0100

    desktop design

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { AppStateProvider } from "./utils/context";
+import '../src/styles/style.scss';
 import App from "./App";
 
 ReactDOM.render(
