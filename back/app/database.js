e6ec23f8ccce939ed2361a0d22b86c83f00fb029
thibault-barrat <contact@thibault-barrat.com>
2022-02-10 20:23:30 +0100

    fix ssl issue with heroku

--- back/app/database.js
+++ back/app/database.js
@@ -5,6 +5,10 @@ const {
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    // on demande à accepter le fait de ne pas être en ssl
+    rejectUnauthorized: false,
+  },
 });
 
 pool.on("error", (err, client) => {
